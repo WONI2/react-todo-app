02e95f0e8857548a555d63050da63afe3feae59b
WONI2 <126743165+WONI2@users.noreply.github.com>
2023-05-25 10:36:31 +0900

    할 일 추가 기능 완성

--- src/component/TodoInput.js
+++ src/component/TodoInput.js
@@ -3,41 +3,57 @@ import { MdAdd } from 'react-icons/md'
 import './scss/TodoInput.scss';
 import cn from 'classnames';
 
-const TodoInput = () => {
+const TodoInput = ({add}) => {
     
     // 입력창이 열리는 여부를 표현하는 값
     const [open, setOpen] = useState(false);
+    
+    // 할일 입력창에 입력한 내용을 표현하는 상태값
+    const [todoText, setTodoText] = useState('');
     
     // 버튼 클릭시 이벤트 처리
     const onToggle = () => {
         setOpen(!open);
     };
-    
-    const showForm = () => {
-     
-        // if(open) {} 였던 것을 표현하는 방법. 안에서 if 문을 사용할 수 없기 떼문에 아래와 같이 표현 할 수 있음
-        // showForm을 사용하지 않고 하는 방법 
-        return open && ( 
-            <div className='form-wrapper'>
-                <form className='insert-form'>
-                       <input 
-                           type='text'
-                           placeholder='할 일을 입력 후, 엔터를 누르세요!'
-                       />
-                </form>
-               </div>
-            );
-        
-    }
+
+    //서브밋 이벤트 핸들러
+    const submitHandler = e => {
+        e.preventDefault(); //태그의 기본기능 제한 
+        // console.log('폼이 제출됨');
+
+        // 바닐라 스크립트에서 사용하던 방법 
+        // const $input = document.querySelector('.insert-form input');
+        // // console.log($input.value);
+        // $input.value = '';
+
+
+        console.log(todoText);
+        // todoTemplate에서 받아온 함수 
+        add(todoText);
+
+        // 입력이 끝나면 입력창 비우기
+        setTodoText('');
+
+
+    };
+    // input change 이벤트 핸들러 함수
+    const todoChangeHandler = e => {
+        // console.log(e.target.value);
+        setTodoText(e.target.value);
+    };
+
 
     return (
     <>
         {
            open && ( <div className='form-wrapper'>
-                <form className='insert-form'>
+                <form className='insert-form' onSubmit={submitHandler}>
                        <input 
                            type='text'
                            placeholder='할 일을 입력 후, 엔터를 누르세요!'
+                           onChange={todoChangeHandler}
+                           autoFocus
+                           value={todoText}
                        />
                 </form>
                </div>)
--- src/component/TodoTemplate.js
+++ src/component/TodoTemplate.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import TodoHeader from './TodoHeader'
 import TodoMain from './TodoMain'
 import TodoInput from './TodoInput'
@@ -7,7 +7,31 @@ import './scss/TodoTemplate.scss';
 const TodoTemplate = () => {
 
     // 서버에 할 일 목록을 요청해서 받아와야 함(json으로)
-    const todos = [
+    // const todos = [
+    //     {
+    //         id : 1,
+    //         title: '산책',
+    //         done: false
+    //     },
+    //     {
+    //         id : 2,
+    //         title: '낮잠자기',
+    //         done: false
+    //     },
+    //     {
+    //         id : 3,
+    //         title: '상추에 물주기',
+    //         done: false
+    //     },
+    //     {
+    //         id : 4,
+    //         title: '감자 캐기',
+    //         done: true
+    //     }
+    // ];
+
+//todos를 상태변수로 만들어줌, todos를 set을 사용해야만 값을 변경할 수 있음 
+    const [todos, setTodos] = useState([
         {
             id : 1,
             title: '산책',
@@ -28,16 +52,48 @@ const TodoTemplate = () => {
             title: '감자 캐기',
             done: true
         }
-    ];
+    ]);
+
+    //  id값 시퀀스 생성함수
+    const makeNewId = () => {
+        if(todos.length === 0 ) return 1;
+        return todos[todos.length-1].id+1;
+    }
+
+// todoInput에게 todoText를 받아오는 함수
+    const addTodo = todoText => {
+        // console.log('할일 정보 in todoTemplate: ',todoText);
+        const newTodo = {
+            id: makeNewId(),
+            title: todoText,
+            done: false
+        };
+
+        // todos.push(newTodo);
+
+        // PUSH가 아니라 setTodos를 통해 다시 초기값으로 해줌. 새로운 것으로 교체해주는 방향으로 해야 한다는 점 기억!
+        // 아래 모두 가능 
+        // setTodos();
+        // setTodos(todos.concat([newTodo]));
+        setTodos([...todos,newTodo]);
 
 
+        // 리액트 상태변수는 무조건 setter를 통해서만 상태값을 변경해야 렌더링에 적용된다
+        // 다만 상태변수가 불변성을 가지기 때문에 기존의 상태에서 변경이 불가능하고
+        // 새로운 상태를 만들어서 변경해야 한다
+    };
+
+    // 렌더링 후 실행되는 함수
+    useEffect(() => {
+        console.log(todos);
+    }, [todos]);
 
 
   return (
     <div className='TodoTemplate'>
         <TodoHeader />
         <TodoMain todoList={todos}/>
-        <TodoInput />
+        <TodoInput add={addTodo}/>
     </div>
   )
 }
