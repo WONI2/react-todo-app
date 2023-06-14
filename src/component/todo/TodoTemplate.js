42380db6ec47b15b544d9b57b58aa493e63f2bf4
WONI2 <126743165+WONI2@users.noreply.github.com>
2023-06-15 08:26:12 +0900

    수정

--- src/component/todo/TodoTemplate.js
+++ src/component/todo/TodoTemplate.js
@@ -2,246 +2,209 @@ import React, { useEffect, useState } from 'react'
 import TodoHeader from './TodoHeader'
 import TodoMain from './TodoMain'
 import TodoInput from './TodoInput'
-import './scss/TodoTemplate.scss';
-import {useNavigate } from 'react-router-dom';
-import {getLoginUserInfo, setLoginUserInfo} from '../../util/login-util'
+import { useNavigate } from 'react-router-dom'
 import { Spinner } from 'reactstrap';
 
-import {API_BASE_URL as BASE, TODO, USER } from '../../config/host-config';
+import './scss/TodoTemplate.scss';
 
+import { API_BASE_URL as BASE, TODO, USER } from '../../config/host-config';
+import { getLoginUserInfo, setLoginUserInfo } from '../../util/login-util'
 
 const TodoTemplate = () => {
 
-    //로딩 상태값 관라
+    const redirection = useNavigate();
+
+    // 로딩 상태값 관리
+    // 로그인 인증토큰 얻어오기
+    const [token, setToken] = useState(getLoginUserInfo().token);
     const [loading, setLoading] = useState(true);
 
 
 
-
-    const redirecte = useNavigate();
-
-    //토큰 얻어오기
-    const [token, setToken] = useState(getLoginUserInfo().token);
-
-    //요청 헤더 설정
+    // 요청 헤더 설정
     const requestHeader = {
-        'content-type' : 'application/json',
-        'Authorization' : 'Bearer '+ token
+      'content-type': 'application/json',
+      'Authorization': 'Bearer ' + token
     };
+    
 
-    // 서버에 할 일 목록을 요청해서 받아와야 함(json으로)
-    // const todos = [
-    //     {
-    //         id : 1,
-    //         title: '산책',
-    //         done: false
-    //     },
-    //     {
-    //         id : 2,
-    //         title: '낮잠자기',
-    //         done: false
-    //     },
-    //     {
-    //         id : 3,
-    //         title: '상추에 물주기',
-    //         done: false
-    //     },
-    //     {
-    //         id : 4,
-    //         title: '감자 캐기',
-    //         done: true
-    //     }
-    // ];
-
-// useEffect : 화면 렌더링 이후 바로 시행되는 ㅎ마수 
-
-    // 서버에 할 일 목록을 요청해서 받아와야 함(json으로)
+    // 서버에 할일 목록(json)을 요청해서 받아와야 함
     const API_BASE_URL = BASE + TODO;
-
     const API_USER_URL = BASE + USER;
-
-
-
-//todos를 상태변수로 만들어줌, todos를 set을 사용해야만 값을 변경할 수 있음 
+    
+    // todos배열을 상태관리
     const [todos, setTodos] = useState([]);
 
-    //  id값 시퀀스 생성함수
-    const makeNewId = () => {
-        if(todos.length === 0 ) return 1;
-        return todos[todos.length-1].id+1;
-    }
+    // id값 시퀀스 생성 함수
+    // const makeNewId = () => {
+    //   if (todos.length === 0) return 1;
+    //   return todos[todos.length - 1].id + 1;
+    // }
 
-// todoInput에게 todoText를 받아오는 함수
+    // TodoInput에게 todoText를 받아오는 함수
     const addTodo = todoText => {
-        // console.log('할일 정보 in todoTemplate: ',todoText);
-        const newTodo = {
+        // console.log('할일 정보 in TodoTemplate: ', todoText);
 
-            // 서버에 보낼 때는 id 와 done만 있으면! 
-            // id: makeNewId(),
-            title: todoText,
-            // done: false
+        const newTodo = {
+          title: todoText
         };
 
         // todos.push(newTodo);
 
-        // PUSH가 아니라 setTodos를 통해 다시 초기값으로 해줌. 새로운 것으로 교체해주는 방향으로 해야 한다는 점 기억!
-        // 아래 모두 가능 
-        // setTodos();
-        // setTodos(todos.concat([newTodo]));
+        // 리액트의 상태변수는 무조건 setter를 통해서만
+        // 상태값을 변경해야 렌더링에 적용된다.
+        // 다만 상태변수가 불변성(immutable)을 가지기 때문에
+        // 기존의 상태에서 변경이 불가능하고
+        // 새로운 상태를 만들어서 변경해야 한다.
+        // const copyTodos = todos.slice();
+        // copyTodos.push(newTodo);
 
         fetch(API_BASE_URL, {
-            method: 'POST',
-            headers: requestHeader,
-            body : JSON.stringify(newTodo)
+          method: 'POST',
+          headers: requestHeader,
+          body: JSON.stringify(newTodo)
         })
         .then(res => {
-            if(res.status === 200 )res.json()
-            else if(res.status === 401){
-                alert('일반회원은 일정등록이 5개로 제한됩니다')
-            }
+          if(res.status === 200) return res.json();
+          else if (res.status === 401) {
+            alert('일반회원은 일정 등록이 5개로 제한됩니다 ㅠㅠ');
+          }
         })
         .then(json => {
-            json && setTodos(json.todos);
+          json && setTodos(json.todos);
         });
-
-        // setTodos([...todos,newTodo]);
-        // 리액트 상태변수는 무조건 setter를 통해서만 상태값을 변경해야 렌더링에 적용된다
-        // 다만 상태변수가 불변성을 가지기 때문에 기존의 상태에서 변경이 불가능하고
-        // 새로운 상태를 만들어서 변경해야 한다
     };
 
 
-
-    // 할일 삭제 대상 아이디 받아서 삭제하는 함수
+    // 할 일 삭제 처리 함수
     const removeTodo = id => {
-        // console.log(`삭제대상 id : ${id}`);
+      // console.log(`삭제대상 id: ${id}`);
+      // setTodos(todos.filter(todo => todo.id !== id));
 
-    //    const copyArr = todos.filter(todo => todo.id !== id);
-    //    setTodos(copyArr);
-
-        fetch(`${API_BASE_URL}/${id}`, {
-            method : 'DELETE',
-            headers : requestHeader
-        })
-        .then(res => res.json())
-        .then(json => {
-          setTodos(json.todos);
-        });
-  
-
-
+      fetch(`${API_BASE_URL}/${id}`, {
+        method: 'DELETE',
+        headers: requestHeader
+      })
+      .then(res => res.json())
+      .then(json => {
+        setTodos(json.todos);
+      });
     };
 
-    // 할일 체크 처리 함수
+    // 할 일 체크 처리 함수
     const checkTodo = (id, done) => {
-        console.log(`체크한 id : ${id}`);
-        
-        // setTodos(todos.map(todo => todo.id === id ? {...todo,done: !todo.done} : todo));
-        
-        fetch(API_BASE_URL, {
-            method: 'PUT',
-            headers: requestHeader,
-            body : JSON.stringify({
-                done: !done,
-                id: id
-            })
+
+      fetch(API_BASE_URL, {
+        method: 'PUT',
+        headers: requestHeader,
+        body: JSON.stringify({
+           done: !done,
+           id: id
         })
-        .then(res => res.json())
-        .then(json => setTodos(json.todos))
+      })
+      .then(res => res.json())
+      .then(json => setTodos(json.todos)); 
+
+      // console.log(`체크한 Todo id: ${id}`);
+
+      // const copyTodos = [...todos];
+      // for (const cTodo of copyTodos) {
+      //   if (cTodo.id === id) {
+      //     cTodo.done = !cTodo.done;
+      //   }
+      // }
+      // setTodos(copyTodos);
+
+      // setTodos(todos.map(todo => todo.id === id ? {...todo, done: !todo.done} : todo));
+
     };
 
+    // 체크가 안된 할 일의 개수 카운트하기
+    const countRestTodo = () => todos.filter(todo => !todo.done).length;
 
 
-    // 체크가 안된 할일의 개수 카운트 학
-    const countRestTodo = () => {
-        const filteredTodo = todos.filter(todo => !todo.done);
-        return filteredTodo.length;
-    };
-
+    // ajax 등급승격 함수
     const fetchPromote = async() => {
-        const res = await fetch( API_USER_URL +'/promote', {
-            method: 'PUT',
-            hearders: requestHeader
-        });
 
-        if(res.status === 403) {
-            alert('이미 프리미엄 회원이거나 관리자 입니다')
-        }else if(res.status === 200) {
-            const json = await res.json();
-            console.log(json); // 새로운 토큰 정보가 전달됨 
-            //토큰갱신
-            setLoginUserInfo(json);
-            setToken(json.token);
-        }
-    }; 
+      const res = await fetch(API_USER_URL + '/promote', {
+        method: 'PUT',
+        headers: requestHeader
+      });
+
+      if (res.status === 403) {
+        alert('이미 프리미엄 회원이거나 관리자입니다.');
+      } else if (res.status === 200) {
+        const json = await res.json();
+        // console.log(json);
+        // 토큰 데이터 갱신
+        setLoginUserInfo(json);
+        setToken(json.token);
+      }
+    };
 
 
-
-
-    //일반회원 클릭 했을 때 프리미엄등급 승격처리
-    const promote =()=> {
-        // console.log('등급 승격 서버요청');
-        fetchPromote();
-    
-    
+    // 프리미엄등급 승격처리
+    const promote = () => {
+      // console.log('등급 승격 서버요청!!');
+      fetchPromote();
     };
 
 
 
-
-
-
-    // 렌더링 후 실행되는 함수
     useEffect(() => {
-        // console.log('잉');
-
-        fetch(API_BASE_URL,{
-            method: 'GET',
-            header: requestHeader
-        })
+      
+      fetch(API_BASE_URL, {
+        method: 'GET',
+        headers: requestHeader
+      })
         .then(res => {
-            if(res.status === 200) return res.json();
-            else if(res.status === 403) {
-                alert('로그인이 필요한 서비스 입니다');
-                redirecte('/login');
-                }else{
-                    alert('서버가 불안정합니다');
-                }
-                return;
-            }
-            )
+          if (res.status === 200) return res.json();
+          else if (res.status === 403) {
+            alert('로그인이 필요한 서비스입니다.');
+            redirection('/login');
+            return;
+            
+          } else {
+            alert('서버가 불안정합니다');
+          }
+        })
         .then(json => {
+          // console.log(json.todos);
 
-            if (!json) return;
-            console.log(json.todos);
-            setTodos(json.todos)
+          if (!json) return;
 
-            //목록불러오기가 끝났을 때  로딩 완료 처리
-            setLoading(false); 
+          setTodos(json.todos);
+
+          // 로딩 완료 처리
+          setLoading(false);
         });
+
     }, []);
 
-// 로딩이 끝난 후 보여줄 컴포넌트
-const loadEndedPage = (
-   <div className='TodoTemplate'>
-     <TodoHeader count={countRestTodo}
-                 promote={promote}    />
-        <TodoMain remove={removeTodo} 
-                  todoList={todos} 
-                  check={checkTodo}/>
-        <TodoInput add={addTodo}/>
-  </div>
-);
-
-// 로딩중일때 보여줄 컴포넌트
-const loadingPage = (
-    <div className='loading'>
-        <Spinner color='yellow'>
-            loading...
-        </Spinner>
+  // 로딩이 끝난 후 보여줄 컴포넌트
+  const loadEndedPage = (
+    <div className='TodoTemplate'>
+        <TodoHeader 
+          count={countRestTodo} 
+          promote={promote}
+        />
+        <TodoMain 
+          todoList={todos} 
+          remove={removeTodo} 
+          check={checkTodo} 
+        />
+        <TodoInput addTodo={addTodo} />   
     </div>
-);
+  );
 
+  // 로딩중일 때 보여줄 컴포넌트
+  const loadingPage = (
+    <div className='loading'>
+      <Spinner color='warning'>
+        loading...
+      </Spinner>
+    </div>
+  );
 
 
   return (
--- src/component/user/Login.js
+++ src/component/user/Login.js
@@ -1,127 +1,128 @@
-import React from 'react'
+import React from 'react';
 import {Grid, Button, Container, Typography, TextField} from "@mui/material";
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { API_BASE_URL as BASE, USER } from '../../config/host-config';
 import { setLoginUserInfo, isLogin } from '../../util/login-util';
 
 const Login = () => {
 
-  
-  const redirect = useNavigate();
-  const REQUEST_URL = BASE + USER + '/signin'; 
+  const redirection = useNavigate();
 
-  if(isLogin()) {
-    alert('이미 로그인 중입니다');
+  if (isLogin()) {
+    alert('이미 로그인 중입니다.');
     window.history.back();
-    return; 
-    //private router 사용하면 공통처리 가능 
+    return;
   }
 
 
-// 서버에 AJAX 요청
-const fetchLogin = async() => {
+  const REQUEST_URL = BASE + USER + '/signin';
 
-    //이메일, 비밀번호 입력태그 얻어오기
+  // 서버에 AJAX요청
+  const fetchLogin = async() => {
+
+    // 이메일, 비밀번호 입력 태그 얻어오기
     const $email = document.getElementById('email');
     const $password = document.getElementById('password');
 
-  const res = await fetch(REQUEST_URL, {
-    method: 'POST',
-    headers: {'content-type' : 'application/json'},
-    body: JSON.stringify({
-      email : $email.value,
-      password : $password.value
-    })
-  });
-  
-  if(res.status === 400 ) {
-    const text = await res.text(); //가입이 안되어 있거나 비밀번호가 틀린경우, 
-        alert(text);                          // 서버에서 온 메세지가 문자열로 저장되어 있기 때문에 text로 받아 줌
-        return; 
-      }
+    const res = await fetch(REQUEST_URL, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({
+        email: $email.value,
+        password: $password.value
+      })
+    });
 
-  const userInfo = await res.json();
-  // console.log(json.userName);
-  // alert(json.userName); 
+    if (res.status === 400) { // 가입이 안되어있거나, 비번틀린 경우
+      const text = await res.text(); // 서버에서 온 문자열 읽기
+      alert(text);
+      return;
+    }
 
-// json에 담긴 인증정보를 클라이언트에 보관
-// 1. 로컬 스토리지 - 브라우저가 종료되어도 보관됨 -- 자동로그인 처럼 클라이언트에서 처리 
-// 2. 세션 스토리지 - 브라우저가 종료되면 사라짐 -- 일반로그인 
+    const userInfo = await res.json(); // 서버에서 온 json 읽기
+    // alert(json.userName);
 
-//1. 로컬스토리지
-setLoginUserInfo(userInfo);
+    // json에 담긴 인증정보를 클라이언트에 보관
+    // 1. 로컬 스토리지 - 브라우저가 종료되어도 보관됨
+    // 2. 세션 스토리지 - 브라우저가 종료되면 사라짐
+    setLoginUserInfo(userInfo);
 
 
+    // 홈으로 리다이렉트
+    redirection('/');
 
-  // 홈으로 리다이렉트
-      redirect('/')
-
-};
-
-
-// 로그인 요청 핸들러
-const loginHandler = e => {
-  e.preventDefault();
+  };
 
 
+  // 로그인 요청 핸들러
+  const loginHandler = e => {
+    e.preventDefault();
 
-  // 서버에 로그인 요청 전송
-  fetchLogin();
+    // 서버에 로그인 요청 전송
+    fetchLogin();
 
-
-};
+  };
 
 
   return (
     <Container component="main" maxWidth="xs" style={{ margin: "200px auto" }}>
-    <Grid container spacing={2}>
-        <Grid item xs={12}>
-            <Typography component="h1" variant="h5">
-                로그인
-            </Typography>
-        </Grid>
-    </Grid>
-    <form noValidate onSubmit={loginHandler}>
-        
         <Grid container spacing={2}>
             <Grid item xs={12}>
-                <TextField
-                    variant="outlined"
-                    required
-                    fullWidth
-                    id="email"
-                    label="email address"
-                    name="email"
-                    autoComplete="email"
-                />
-            </Grid>
-            <Grid item xs={12}>
-                <TextField
-                    variant="outlined"
-                    required
-                    fullWidth
-                    name="password"
-                    label="on your password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                />
-            </Grid>
-            <Grid item xs={12}>
-                <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                >
+                <Typography component="h1" variant="h5">
                     로그인
-                </Button>
+                </Typography>
             </Grid>
         </Grid>
-    </form>
-</Container>
+
+        <form noValidate onSubmit={loginHandler}>
+            
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <TextField
+                        variant="outlined"
+                        required
+                        fullWidth
+                        id="email"
+                        label="email address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        variant="outlined"
+                        required
+                        fullWidth
+                        name="password"
+                        label="on your password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        style={{background: '#38d9a9'}}
+                    >
+                        로그인
+                    </Button>
+                </Grid>
+                <Grid item>
+                    <Link to="/join">
+                        회원가입을 통해 서비스를 이용하세요.
+                    </Link>
+                </Grid>
+                <Grid container justify="flex-end">
+            </Grid>
+            </Grid>
+        </form>
+    </Container>
   )
 }
 
-export default Login
+export default Login;
